0fb221048767d96183a05d41df3c3d1898908006
Sumit Kumar <sumitkumar124@live.com>
2023-10-09 18:06:32 +0530

    Update Sidenav.js
    href

--- src/compo/Sidenav.js
+++ src/compo/Sidenav.js
@@ -15,7 +15,7 @@ const Sidenav = () => {
         <img src="./logo.svg" className="logo" alt="website logo" />
         <ul className="ul">
           <div className={linkClassName}>
-            <a className="closebtn linkA" onClick={handleClick}>
+            <a href="" className="closebtn linkA" onClick={handleClick}>
               &times;
             </a>
             <Link className="linkA" to="/">
